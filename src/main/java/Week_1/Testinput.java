64af9e12cc9eb943fe47174c73cc984c95a242ab
Nerple <fb3388hc@>
2024-01-23 10:48:02 -0600

    Initial commit

---
+++ src/main/java/Week_1/Testinput.java
@@ -0,0 +1,16 @@
+package Week_1;
+
+import static input.InputUtils.stringInput;
+
+public class Testinput {
+
+    //psyn shortcut
+
+    public static void main(String[] args) {
+
+        String name = stringInput("Enter your name: ");
+        System.out.println("Hello " + name);
+
+        //if you look up solutions put the source as a citation in notes
+    }
+}
